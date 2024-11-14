d255dc2e75718c6bc21995063bcb4b4e2592b1d7
Yushirizu <la229303@student.helha.be>
2024-11-14 12:31:20 +0100

    KYS

--- industrial-project/src/appBar.tsx
+++ industrial-project/src/appBar.tsx
@@ -5,15 +5,6 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import * as React from "react";
-import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import Switch from "@mui/material/Switch";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import FormGroup from "@mui/material/FormGroup";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-import { useSession } from "next-auth/react";
-import { Avatar } from "@mui/material";
 
 const pages = [
 	{ name: "Index", href: "/" },
@@ -22,17 +13,6 @@ const pages = [
 ];
 
 function ResponsiveAppBar() {
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	// const { data: session } = useSession();
-
-	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
-
 	return (
 		<AppBar
 			position="static"
@@ -55,46 +35,6 @@ function ResponsiveAppBar() {
 							</Button>
 						))}
 					</Box>
-					{/* {session && (
-						<div>
-							<IconButton
-								size="large"
-								aria-label="account of current user"
-								aria-controls="menu-appbar"
-								aria-haspopup="true"
-								onClick={handleMenu}
-								color="inherit"
-							>
-								avatar=
-								{session?.user.image ? (
-									<Avatar
-										alt={session.user.name || ""}
-										src={session.user.image || ""}
-									/>
-								) : (
-									<Avatar>{session?.user.name?.charAt(0)}</Avatar>
-								)}
-							</IconButton>
-							<Menu
-								id="menu-appbar"
-								anchorEl={anchorEl}
-								anchorOrigin={{
-									vertical: "top",
-									horizontal: "right",
-								}}
-								keepMounted
-								transformOrigin={{
-									vertical: "top",
-									horizontal: "right",
-								}}
-								open={Boolean(anchorEl)}
-								onClose={handleClose}
-							>
-								<MenuItem onClick={handleClose}>Profile</MenuItem>
-								<MenuItem onClick={handleClose}>My account</MenuItem>
-							</Menu>
-						</div>
-					)} */}
 				</Toolbar>
 			</Container>
 		</AppBar>
