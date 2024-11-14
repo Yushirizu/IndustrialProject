09cb3f7b53601431a02635ce3d176d1c0cc94cea
Didinozor <diego-napolitano@hotmail.com>
2024-11-14 11:51:18 +0100

    DISCORD

--- industrial-project/src/pages/index.tsx
+++ industrial-project/src/pages/index.tsx
@@ -25,20 +25,50 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container maxWidth="xl">
-        {!session ? (
-          <Button variant="contained" onClick={() => signIn()} color="primary">
-            Essai login
-          </Button>
-        ) : (
-          <Button variant="contained" onClick={() => signOut()} color="primary">
-            Essai logout
-          </Button>
-        )}
-
-        <Typography variant="body1">
-          Connecté en tant que: {session?.user.name}
-        </Typography>
+      <Container maxWidth="xl" sx={{ mt: 4 }}>
+        <Card>
+          <CardHeader
+            avatar={
+              session?.user.image ? (
+                <Avatar
+                  alt={session.user.name || ""}
+                  src={session.user.image || ""}
+                />
+              ) : (
+                <Avatar>{session?.user.name?.charAt(0)}</Avatar>
+              )
+            }
+            action={
+              !session ? (
+                <Button
+                  variant="contained"
+                  onClick={() => signIn()}
+                  color="primary">
+                  Essai login
+                </Button>
+              ) : (
+                <Button
+                  variant="contained"
+                  onClick={() => signOut()}
+                  color="primary">
+                  Essai logout
+                </Button>
+              )
+            }
+            title={
+              session
+                ? `Connecté en tant que: ${session.user.name}`
+                : "Non connecté"
+            }
+          />
+          <CardContent>
+            <Typography variant="body1">
+              {session
+                ? `Bienvenue, ${session.user.name}!`
+                : "Veuillez vous connecter."}
+            </Typography>
+          </CardContent>
+        </Card>
       </Container>
     </>
   );
