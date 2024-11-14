7c6af476435ad325ee9d5f73f6a49709c90df237
Yushirizu <la229303@student.helha.be>
2024-11-14 11:19:37 +0100

    yac

--- industrial-project/src/pages/_app.tsx
+++ industrial-project/src/pages/_app.tsx
@@ -1,21 +1,33 @@
-import { GeistSans } from "geist/font/sans";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 
-import "~/styles/globals.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <div className={GeistSans.className}>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</SessionProvider>
+	);
 };
 
 export default MyApp;
