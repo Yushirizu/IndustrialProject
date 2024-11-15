5ae332b689994913a779ff95ea7542411c5856f8
Yushirizu <la229303@student.helha.be>
2024-11-15 10:34:30 +0100

    la db lo

--- industrial-project/src/pages/api/getLive.ts
+++ industrial-project/src/pages/api/getLive.ts
@@ -3,10 +3,9 @@ import { db } from "~/server/db";
 
 export default function getRelais(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === "POST") {
-		// Récupération des relais
-		// db.relai.findMany().then((relais) => {
-		// 	res.status(200).json(relais);
-		// });
+		db.live.findMany().then((msg) => {
+			res.status(200).json(msg);
+		});
 		console.log("GET /api/getLive");
 	} else {
 		res.status(404).send("Not Found");
--- industrial-project/src/server/fetcher.ts
+++ industrial-project/src/server/fetcher.ts
@@ -1,4 +1,3 @@
-import { object } from "zod";
 import { db } from "./db";
 
 const ws: WebSocket = new WebSocket("ws://127.0.0.1:1881/ws/yape");
@@ -9,10 +8,37 @@ export function initWS() {
 	});
 
 	ws.addEventListener("message", (msg) => {
-		// console.log("Message reçu: " + msg.data);
-	});
+		console.log(msg.data);
+		const data = JSON.parse(msg.data);
 
-	ws.addEventListener("error", (event) => {
-		console.log("Erreur websocket");
+		// Vérifiez que toutes les valeurs nécessaires sont présentes
+		if (
+			data.volt !== undefined &&
+			data.air !== undefined &&
+			data.current !== undefined &&
+			data.ActivePower !== undefined &&
+			data.PowerFactor !== undefined &&
+			data.EnergyConsumed !== undefined &&
+			data.FeedCapCarre !== undefined &&
+			data.FeedCapRound !== undefined
+		) {
+			db.live
+				.create({
+					data: {
+						volt: data.volt,
+						air: data.air,
+						current: data.current,
+						ActivePower: data.ActivePower,
+						PowerFactor: data.PowerFactor,
+						EnergyConsumed: data.EnergyConsumed,
+						FeedCapCarre: data.FeedCapCarre,
+						FeedCapRound: data.FeedCapRound,
+					},
+				})
+				.then(() => console.log("Ajouté"))
+				.catch((error) => console.error("Erreur lors de l'ajout:", error));
+		} else {
+			console.error("Données manquantes ou invalides:", data);
+		}
 	});
 }
