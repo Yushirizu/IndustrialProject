9c7c614566c572e67717e346c3a1c2a0e16b7152
Didinozor <diego-napolitano@hotmail.com>
2024-12-16 10:00:31 +0100

    c un pe plus joli

--- industrial-project/src/pages/live.tsx
+++ industrial-project/src/pages/live.tsx
@@ -8,151 +8,171 @@ import Grid from "@mui/material/Grid2";
 import { useSession } from "next-auth/react";
 
 export default function Live() {
-	const { data: session, status } = useSession() as {
-		data: { user: { isAdmin: boolean } } | null;
-		status: string;
-	};
-	const [liveData, setLiveData] = useState<any>(null);
-	const [error, setError] = useState<string | null>(null);
-	const [isAdmin, setIsAdmin] = useState<boolean>(false);
-	let receivedData: { [key: string]: number } = {};
+  const { data: session, status } = useSession() as {
+    data: { user: { isAdmin: boolean } } | null;
+    status: string;
+  };
+  const [liveData, setLiveData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [isAdmin, setIsAdmin] = useState<boolean>(false);
+  let receivedData: { [key: string]: number } = {};
 
-	useEffect(() => {
-		if (status === "authenticated") {
-			let ws: WebSocket;
+  useEffect(() => {
+    if (status === "authenticated") {
+      let ws: WebSocket;
 
-			const connectWebSocket = () => {
-				ws = new WebSocket("ws://127.0.0.1:1881/ws/live");
+      const connectWebSocket = () => {
+        ws = new WebSocket("ws://192.168.200.248:1881/ws/live");
 
-				ws.addEventListener("open", () => {
-					console.log("WebSocket connection established");
-				});
+        ws.addEventListener("open", () => {
+          console.log("WebSocket connection established");
+        });
 
-				ws.addEventListener("message", (msg) => {
-					try {
-						const data = JSON.parse(msg.data);
-						receivedData[data.topic] = data.payload;
-						const requiredKeys = [
-							"volt",
-							"air",
-							"current",
-							"ActivePower",
-							"PowerFactor",
-							"EC",
-							"FCC",
-							"FCR",
-						];
+        ws.addEventListener("message", (msg) => {
+          try {
+            const data = JSON.parse(msg.data);
+            receivedData[data.topic] = data.payload;
+            const requiredKeys = [
+              "volt",
+              "air",
+              "current",
+              "ActivePower",
+              "PowerFactor",
+              "EC",
+              "FCC",
+              "FCR",
+            ];
 
-						if (requiredKeys.every((key) => key in receivedData)) {
-							const {
-								volt,
-								air,
-								current,
-								ActivePower,
-								PowerFactor,
-								EC: EnergyConsumed,
-								FCC: FeedCapCarre,
-								FCR: FeedCapRound,
-							} = receivedData;
+            if (requiredKeys.every((key) => key in receivedData)) {
+              const {
+                volt,
+                air,
+                current,
+                ActivePower,
+                PowerFactor,
+                EC: EnergyConsumed,
+                FCC: FeedCapCarre,
+                FCR: FeedCapRound,
+              } = receivedData;
 
-							// Réinitialisez le tableau pour les prochaines données
-							receivedData = {};
+              // Réinitialisez le tableau pour les prochaines données
+              receivedData = {};
 
-							// Mettez à jour l'état avec les nouvelles données
-							setLiveData({
-								volt,
-								air,
-								current,
-								ActivePower,
-								PowerFactor,
-								EnergyConsumed,
-								FeedCapCarre,
-								FeedCapRound,
-							});
-						}
-					} catch (error) {
-						console.error("Invalid JSON data received:", msg.data);
-					}
-				});
+              // Mettez à jour l'état avec les nouvelles données
+              setLiveData({
+                volt,
+                air,
+                current,
+                ActivePower,
+                PowerFactor,
+                EnergyConsumed,
+                FeedCapCarre,
+                FeedCapRound,
+              });
+            }
+          } catch (error) {
+            console.error("Invalid JSON data received:", msg.data);
+          }
+        });
 
-				ws.addEventListener("error", (err) => {
-					console.error("WebSocket error:", err);
-					setError(err.toString());
-				});
+        ws.addEventListener("error", (err) => {
+          console.error("WebSocket error:", err);
+          setError(err.toString());
+        });
 
-				ws.addEventListener("close", () => {
-					console.log("WebSocket connection closed");
-					// Reconnecter après une fermeture
-					setTimeout(connectWebSocket, 1000);
-				});
-			};
+        ws.addEventListener("close", () => {
+          console.log("WebSocket connection closed");
+          // Reconnecter après une fermeture
+          setTimeout(connectWebSocket, 1000);
+        });
+      };
 
-			connectWebSocket();
+      connectWebSocket();
 
-			return () => {
-				if (ws) {
-					ws.close();
-				}
-			};
-		}
-	}, [status]);
+      return () => {
+        if (ws) {
+          ws.close();
+        }
+      };
+    }
+  }, [status]);
 
-	if (status === "loading") {
-		return <div>Loading...</div>;
-	}
+  if (status === "loading") {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}>
+        Loading...
+      </div>
+    );
+  }
 
-	if (status === "unauthenticated") {
-		return <div>Please sign in to view this page.</div>;
-	}
+  if (status === "unauthenticated") {
+    return <div>Please sign in to view this page.</div>;
+  }
 
-	if (error) {
-		return <div>{error}</div>;
-	}
+  if (error) {
+    return <div>{error}</div>;
+  }
 
-	if (!liveData) {
-		return <div>Loading data...</div>;
-	}
+  if (!liveData) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}>
+        Loading data...
+      </div>
+    );
+  }
 
-	return (
-		<>
-			<Head>
-				<title>Live Data</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Live Data</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<Container maxWidth="xl" sx={{ mt: 4 }}>
-				<Grid container spacing={4}>
-					<Grid size={6}>
-						<Card>
-							<CardHeader title="Power" />
-							<CardContent>
-								<p>Voltage: {liveData.volt}</p>
-								<p>Current: {liveData.current}</p>
-								<p>Active Power: {liveData.ActivePower}</p>
-								<p>Power Factor: {liveData.PowerFactor}</p>
-							</CardContent>
-						</Card>
-					</Grid>
-					<Grid size={6}>
-						<Card>
-							<CardHeader title="Consumption" />
-							<CardContent>
-								<p>Energy Consumed: {liveData.EnergyConsumed}</p>
-								<p>Air: {liveData.air}</p>
-							</CardContent>
-						</Card>
-					</Grid>
-					<Grid size={6}>
-						<Card>
-							<CardHeader title="Caps" />
-							<CardContent>
-								<p>Feed Cap Carre: {liveData.FeedCapCarre}</p>
-								<p>Feed Cap Round: {liveData.FeedCapRound}</p>
-							</CardContent>
-						</Card>
-					</Grid>
-				</Grid>
-			</Container>
-		</>
-	);
+      <Container maxWidth="xl" sx={{ mt: 4 }}>
+        <Grid container spacing={4}>
+          <Grid size={6}>
+            <Card sx={{ p: 2, boxShadow: 3, borderRadius: 5 }}>
+              <CardHeader title="Power" />
+              <CardContent>
+                <p>Voltage: {liveData.volt}</p>
+                <p>Current: {liveData.current}</p>
+                <p>Active Power: {liveData.ActivePower}</p>
+                <p>Power Factor: {liveData.PowerFactor}</p>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={6}>
+            <Card sx={{ p: 2, boxShadow: 3, borderRadius: 5 }}>
+              <CardHeader title="Consumption" />
+              <CardContent>
+                <p>Energy Consumed: {liveData.EnergyConsumed}</p>
+                <p>Air: {liveData.air}</p>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={6}>
+            <Card sx={{ p: 2, boxShadow: 3, borderRadius: 5 }}>
+              <CardHeader title="Caps" />
+              <CardContent>
+                <p>Feed Cap Square: {liveData.FeedCapCarre}</p>
+                <p>Feed Cap Round: {liveData.FeedCapRound}</p>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
