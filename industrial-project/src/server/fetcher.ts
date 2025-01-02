ebc0fe885d7cd6da8bf5f71a07387fd36698e2ca
Yushirizu <la229303@student.helha.be>
2025-01-02 15:39:17 +0100

    Refactor fetcher and strucTables for improved readability and structure

--- industrial-project/src/server/fetcher.ts
+++ industrial-project/src/server/fetcher.ts
@@ -1,100 +1,42 @@
-import { db } from "./db";
-
-interface LiveData {
-	volt: number;
-	air: number;
-	current: number;
-	ActivePower: number;
-	PowerFactor: number;
-	EC: number;
-	FCC: number;
-	FCR: number;
-}
-
-let receivedData: LiveData = {
-	volt: 0,
-	air: 0,
-	current: 0,
-	ActivePower: 0,
-	PowerFactor: 0,
-	EC: 0,
-	FCC: 0,
-	FCR: 0,
-};
+import {db} from "./db";
 
 const ws: WebSocket = new WebSocket("ws://130.130.130.245:1880/ws/live");
 
+let receivedData: Record<string, number> = {};
+
 export function initWS() {
-	ws.addEventListener("open", () => {
-		console.log("WebSocket connection established");
-	});
+    ws.addEventListener("open", () => {
+        console.log("Websocket ouvert");
+    });
 
-	ws.addEventListener("message", (msg) => {
-		try {
-			let data: { topic: string; payload: number };
-			data = JSON.parse(msg.data);
-			if (typeof data.topic === "string" && typeof data.payload === "number") {
-				receivedData[data.topic as keyof LiveData] = data.payload;
-			}
+    ws.addEventListener("message", () => {
+        const requiredKeys = ["volt", "air", "current", "ActivePower", "PowerFactor", "EC", "FCC", "FCR"];
 
-			const requiredKeys: (keyof LiveData)[] = [
-				"volt",
-				"air",
-				"current",
-				"ActivePower",
-				"PowerFactor",
-				"EC",
-				"FCC",
-				"FCR",
-			];
+        if (requiredKeys.every((key) => key in receivedData)) {
+            const {
+                volt, air, current, ActivePower, PowerFactor, EC: EnergyConsumed, FCC: FeedCapCarre, FCR: FeedCapRound,
+            } = receivedData;
 
-			if (requiredKeys.every((key) => key in receivedData)) {
-				const {
-					volt,
-					air,
-					current,
-					ActivePower,
-					PowerFactor,
-					EC: EnergyConsumed,
-					FCC: FeedCapCarre,
-					FCR: FeedCapRound,
-				} = receivedData;
+            // Réinitialisez le tableau pour les prochaines données
+            receivedData = {};
 
-				receivedData = {
-					volt: 0,
-					air: 0,
-					current: 0,
-					ActivePower: 0,
-					PowerFactor: 0,
-					EC: 0,
-					FCC: 0,
-					FCR: 0,
-				};
-
-				db.historicalData.create({
-					data: {
-						timestamp: new Date(),
-						volt: Number(volt),
-						air: Number(air),
-						current: Number(current),
-						ActivePower: Number(ActivePower),
-						PowerFactor: Number(PowerFactor),
-						EnergyConsumed: Number(EnergyConsumed),
-						FeedCapCarre: Number(FeedCapCarre),
-						FeedCapRound: Number(FeedCapRound),
-					},
-				});
-			}
-		} catch (error) {
-			console.error("Données reçues ne sont pas un objet JSON:", msg.data);
-		}
-	});
-
-	ws.addEventListener("error", (err) => {
-		console.error("WebSocket error:", err);
-	});
-
-	ws.addEventListener("close", () => {
-		console.log("WebSocket connection closed");
-	});
+            // Insérez les données dans la base de données
+            db.historicalData
+                .create({
+                    data: {
+                        timestamp: new Date(),
+                        volt: Number(volt),
+                        air: Number(air),
+                        current: Number(current),
+                        ActivePower: Number(ActivePower),
+                        PowerFactor: Number(PowerFactor),
+                        EnergyConsumed: Number(EnergyConsumed),
+                        FeedCapCarre: Number(FeedCapCarre),
+                        FeedCapRound: Number(FeedCapRound),
+                    },
+                })
+                .then(() => console.log("Ajouté"))
+                .catch((error: unknown) => console.error("Erreur lors de l'ajout:", error));
+        }
+    });
 }
--- industrial-project/src/strucTables.ts
+++ industrial-project/src/strucTables.ts
@@ -1,11 +1,7 @@
 import {GridColDef} from "@mui/x-data-grid";
 
 export const tableLive: GridColDef[] = [{
-    field: "id",
-    headerName: "ID",
-    width: 100,
-    align: "center",
-    headerAlign: "center"
+    field: "id", headerName: "ID", width: 100, align: "center", headerAlign: "center"
 }, {
     field: "timestamp",
     headerName: "Timestamp",
@@ -17,33 +13,13 @@ export const tableLive: GridColDef[] = [{
         return date.toLocaleString();
     }
 }, {field: "volt", headerName: "Volt", width: 200, align: "center", headerAlign: "center"}, {
-    field: "air",
-    headerName: "Air consumption",
-    width: 100,
-    align: "center",
-    headerAlign: "center"
+    field: "air", headerName: "Air consumption", width: 100, align: "center", headerAlign: "center"
 }, {field: "current", headerName: "Current", width: 200, align: "center", headerAlign: "center"}, {
-    field: "ActivePower",
-    headerName: "ActivePower",
-    width: 100,
-    align: "center",
-    headerAlign: "center"
+    field: "ActivePower", headerName: "ActivePower", width: 100, align: "center", headerAlign: "center"
 }, {
-    field: "PowerFactor",
-    headerName: "PowerFactor",
-    width: 200,
-    align: "center",
-    headerAlign: "center"
+    field: "PowerFactor", headerName: "PowerFactor", width: 200, align: "center", headerAlign: "center"
 }, {
-    field: "EnergyConsumed",
-    headerName: "EnergyConsumed",
-    width: 100,
-    align: "center",
-    headerAlign: "center"
+    field: "EnergyConsumed", headerName: "EnergyConsumed", width: 100, align: "center", headerAlign: "center"
 }, {
-    field: "FeedCapCarre",
-    headerName: "FeedCapCarre",
-    width: 125,
-    align: "center",
-    headerAlign: "center"
+    field: "FeedCapCarre", headerName: "FeedCapCarre", width: 125, align: "center", headerAlign: "center"
 }, {field: "FeedCapRound", headerName: "FeedCapRound", width: 125, align: "center", headerAlign: "center"},];
